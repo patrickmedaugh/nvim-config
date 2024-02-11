33dc53f19550036f8420c6103d13f62e1dcc03d3
roy <troika@fens-MacBook-Pro.local>
2024-02-11 15:14:17 -0700

    Remove custom from gitignore, add keymap to custom config that opens urls with gx

---
+++ lua/custom/mappings.lua
@@ -0,0 +1,43 @@
+---@type MappingsTable
+local M = {}
+
+--vim.api.nvim_set_keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
+
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter cmdline", opts = { nowait = true } },
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    },
+  },
+  v = {
+    [">"] = { ">gv", "indent" },
+  },
+}
+
+-- more keybinds!
+
+-- sets gx to open urls
+local open_command = 'xdg-open'
+if vim.fn.has('mac') == 1 then
+  open_command = 'open'
+end
+
+local function url_repo()
+  local cursorword = vim.fn.expand('<cfile>')
+  if string.find(cursorword, '^[a-zA-Z0-9-_.]*/[a-zA-Z0-9-_.]*$') then
+    cursorword = 'https://github.com/' .. cursorword
+  end
+  return cursorword or ''
+end
+
+vim.keymap.set('n', 'gx', function()
+  vim.fn.jobstart({ open_command, url_repo() }, { detach = true })
+end, { silent = true })
+
+return M
